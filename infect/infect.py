8c2c0a62fa2179c71f8263873bc86238bcbb6fab
Lowe Thiderman <lowe.thiderman@gmail.com>
2013-12-08 15:45:57 +0100

    Add first skeleton of infect file
    Also add bin/infect

---
+++ infect/infect.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+
+# import argparse
+
+
+class Infect(object):
+    def __init__(self):
+        pass
+
+    def install(self):
+        pass
+
+    def upload(self):
+        pass
+
+    def symlink(self):
+        pass
+
+    def uninstall(self):
+        pass
+
+
+def main():
+    print('Running infect main()')
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
