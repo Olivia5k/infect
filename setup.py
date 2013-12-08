1cbb168227b11dc1a4734c2466ff89ebfdf82b27
Lowe Thiderman <lowe.thiderman@gmail.com>
2013-12-08 15:28:05 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='infect',
+    version='0.0.1',
+    url='https://github.com/thiderman/infect',
+    author='Lowe Thiderman, Anton Lazarew',
+    author_email='lowe.thiderman@gmail.com, tony@lazarew.me',
+    description=('dotfiles distribution management'),
+    license='MIT',
+    packages=['infect'],
+    scripts=[
+        'bin/infect'
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+    ],
+)
