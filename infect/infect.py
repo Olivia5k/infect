e9df7e663213e878f365440d53cbd469827eb013
Lowe Thiderman <lowe.thiderman@gmail.com>
2013-12-11 01:49:34 +0100

    Refactor symlinker to use Exceptions for flow
    Static strings? Never code while feverish.

--- infect/infect.py
+++ infect/infect.py
@@ -8,14 +8,6 @@ import os
 
 
 class Infect(object):
-    class Codes:
-        symlink = {
-            'success': 0,
-            'target_not_found': 1,
-            'destination_already_linked': 2,
-            'destination_exists': 3,
-        }
-
     def __init__(self):
         pass
 
@@ -29,22 +21,17 @@ class Infect(object):
         """
         Symlink a file and its destination
 
-        Will return a return code from `Infect.Codes.symlink`.
+        Will raise OSErrors if the source is missing or if the destination
+        already exists. Check the exception for the `errno` and `filename`
+        attributes to determine what went wrong. `errno == 17` means that the
+        destination existed and `errno == 2` means that the target was not
+        found.
 
         """
 
         if not os.path.isfile(target):
-            return self.Codes.symlink['target_not_found']
-
-        if os.path.islink(dest):
-            return self.Codes.symlink['destination_already_linked']
-
-        if os.path.isfile(dest) or os.path.isdir(dest):
-            return self.Codes.symlink['destination_exists']
-
+            raise OSError(2, 'target file not found', target)
         os.symlink(target, dest)
-
-        return self.Codes.symlink['success']
 
     def uninstall(self):  # pragma: nocover
         pass
--- test/test_infect.py
+++ test/test_infect.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os
 import tempfile
 import shutil
-import os
-from os import path
+import pytest
 
 from infect.infect import Infect
 
@@ -20,15 +20,13 @@ class TestSymlink(object):
 
     def setup_method(self, method):
         self.infect = Infect()
-        self.codes = self.infect.Codes.symlink
-
         self.tempdir = tempfile.mkdtemp(
             prefix='infect-{0}-'.format(method.__name__)
         )
 
         # Store [1] to get the name only
         self.target = tempfile.mkstemp(dir=self.tempdir)[1]
-        self.dest = path.join(self.tempdir, 'target')
+        self.dest = os.path.join(self.tempdir, 'target')
 
     def teardown_method(self, method):
         # Try to tear down the temporary directory. Don't care if it fails.
@@ -38,35 +36,42 @@ class TestSymlink(object):
             pass
 
     def test_target_not_present(self):
-        target = path.join(self.tempdir, 'nope')
-        ret = self.infect.symlink(target, self.dest)
+        target = os.path.join(self.tempdir, 'nope')
+        with pytest.raises(OSError) as exc:
+            self.infect.symlink(target, self.dest)
 
-        assert ret == self.codes['target_not_found']
-        assert not path.islink(self.dest)
+        assert exc.value.errno == 2
+        assert exc.value.filename == target
+        assert not os.path.islink(self.dest)
 
     def test_target_present_and_dest_does_not_exist(self):
-        ret = self.infect.symlink(self.target, self.dest)
+        self.infect.symlink(self.target, self.dest)
 
-        assert ret == self.codes['success']
-        assert path.islink(self.dest)
+        assert os.path.islink(self.dest)
 
     def test_target_present_and_dest_is_a_link(self):
         os.symlink(self.target, self.dest)
-        ret = self.infect.symlink(self.target, self.dest)
+        with pytest.raises(OSError) as exc:
+            self.infect.symlink(self.target, self.dest)
 
-        assert ret == self.codes['destination_already_linked']
-        assert path.islink(self.dest)
+        assert exc.value.errno == 17
+        assert exc.value.filename == self.dest
+        assert os.path.islink(self.dest)
 
     def test_target_present_and_dest_is_a_file(self):
         dest = tempfile.mkstemp(dir=self.tempdir)[1]
-        ret = self.infect.symlink(self.target, dest)
+        with pytest.raises(OSError) as exc:
+            self.infect.symlink(self.target, dest)
 
-        assert ret == self.codes['destination_exists']
-        assert not path.islink(self.dest)
+        assert exc.value.errno == 17
+        assert exc.value.filename == dest
+        assert not os.path.islink(self.dest)
 
     def test_target_present_and_dest_is_a_directory(self):
         dest = tempfile.mkdtemp(dir=self.tempdir)
-        ret = self.infect.symlink(self.target, dest)
+        with pytest.raises(OSError) as exc:
+            self.infect.symlink(self.target, dest)
 
-        assert ret == self.codes['destination_exists']
-        assert not path.islink(self.dest)
+        assert exc.value.errno == 17
+        assert exc.value.filename == dest
+        assert not os.path.islink(self.dest)
